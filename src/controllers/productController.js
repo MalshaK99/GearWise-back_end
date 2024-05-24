ab539a003e5ee06f06f830b2d324c8e56a72fdfa
Malsha Karunarathna <cst20060@std.uwu.ac.lk>
2024-05-24 22:17:54 +0530

    uppp

---
+++ src/controllers/productController.js
@@ -0,0 +1,71 @@
+// controllers/productController.js
+
+const Product = require("../models/product");
+
+// Create a new product
+exports.createProduct = async (req, res) => {
+    console.log(req.body);
+    const product = new Product(req.body);
+
+    try {
+        await product.save();
+        res.status(201).send(product);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+// Get all products
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).send(products);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+// Get a product by ID
+exports.getProductById = async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const product = await Product.findById(_id);
+        if (!product) {
+            return res.status(404).send();
+        }
+        res.status(200).send(product);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+// Update a product by ID
+exports.updateProductById = async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(_id, req.body, { new: true });
+        if (!updatedProduct) {
+            return res.status(404).send();
+        }
+        res.status(200).send(updatedProduct);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+// Delete a product by ID
+exports.deleteProductById = async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(_id);
+        if (!deletedProduct) {
+            return res.status(404).send();
+        }
+        res.status(200).send(deletedProduct);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,73 +1,22 @@
+// routes/productRoutes.js
+
 const express = require("express");
 const router = express.Router();
-const Product = require("../models/product");
+const productController = require("../controllers/productController");
 
 // Create a new product
-router.post("/products", async (req, res) => {
-    console.log(req.body);
-    const product = new Product(req.body);
-
-    try {
-        await product.save();
-        res.status(201).send(product);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
+router.post("/products", productController.createProduct);
 
 // Get all products
-router.get("/products", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).send(products);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
+router.get("/products", productController.getAllProducts);
 
 // Get a product by ID
-router.get("/products/:id", async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const product = await Product.findById(_id);
-        if (!product) {
-            return res.status(404).send();
-        }
-        res.status(200).send(product);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
+router.get("/products/:id", productController.getProductById);
 
 // Update a product by ID
-router.patch("/products/:id", async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(_id, req.body, { new: true });
-        if (!updatedProduct) {
-            return res.status(404).send();
-        }
-        res.status(200).send(updatedProduct);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
+router.patch("/products/:id", productController.updateProductById);
 
 // Delete a product by ID
-router.delete("/products/:id", async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const deletedProduct = await Product.findByIdAndDelete(_id);
-        if (!deletedProduct) {
-            return res.status(404).send();
-        }
-        res.status(200).send(deletedProduct);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
+router.delete("/products/:id", productController.deleteProductById);
 
 module.exports = router;
