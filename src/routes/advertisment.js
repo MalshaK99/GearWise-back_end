d385573b689d343c32403a8a411460381ad1fa0a
Malsha Karunarathna <cst20060@std.uwu.ac.lk>
2024-05-24 22:14:18 +0530

    upp

--- src/routes/advertisment.js
+++ src/routes/advertisment.js
@@ -1,8 +1,10 @@
+// routes/advertisementRoutes.js
+
 const express = require("express");
 const router = express.Router();
-const Ad = require("../models/advertisment");
 const multer = require("multer");
 const path = require("path");
+const advertisementController = require("../controllers/adController");
 
 // Multer configuration
 const storage = multer.diskStorage({
@@ -17,31 +19,9 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Create a new ad
-router.post("/ad", upload.single("adImage"), async (req, res) => {
-    try {
-        const ad = new Ad({
-            name: req.body.name,
-            email: req.body.email,
-            phone: req.body.phone,
-            imagePath: req.file.path, // Save the path of the uploaded image
-            description: req.body.description
-        });
+router.post("/ad", upload.single("adImage"), advertisementController.createAdvertisement);
 
-        await ad.save();
-        res.status(201).send(ad);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
-  
-// Get all products
-router.get("/ad", async (req, res) => {
-    try {
-        const ad = await Ad.find({});
-        res.status(200).send(ad);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
+// Get all advertisements
+router.get("/ad", advertisementController.getAllAdvertisements);
 
 module.exports = router;
