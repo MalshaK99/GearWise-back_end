78d86e9863f294008e3c65ed3accf9e124d8eca1
Malsha Karunarathna <cst20060@std.uwu.ac.lk>
2024-05-28 20:09:47 +0530

    updatee

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -7,8 +7,10 @@ const customerController = require("../controllers/customerController");
 
 // Get all customers
 router.get("/customer", customerController.getAllCustomers);
+
 //if not relevant this del this(created to test)
 router.post("/customer", customerController.createCustomer);
+
 //activate-deactivate customers
 router.put('/customer/:customerId/toggle-status', customerController.toggleCustomerStatus);
 
