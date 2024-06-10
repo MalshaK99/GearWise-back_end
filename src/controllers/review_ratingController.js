b1fc8ccd60a4483f6192b6d354def3538183e6b0
Hasanki Dharmarathna <didulanidharmarathna@gmail.com>
2024-06-10 12:53:36 +0530

    fetching reviews

--- src/controllers/review_ratingController.js
+++ src/controllers/review_ratingController.js
@@ -14,4 +14,17 @@ exports.addReview = async (req, res) => {
         res.status(400).send(error);
     }
 };
- 
+
+
+//get all review and ratings
+exports.getReviews = async (req, res) => {
+    // console.log(req.body);
+    // const review = new review(req.body);
+
+    try {
+        const reviews = await Review_ratings.find({});
+        res.status(201).send(reviews);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
