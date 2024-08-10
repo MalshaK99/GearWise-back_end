002b7cff0e7aaeb03cef9be341824f1cfb0a69b8
Malith Promoddaya Rathnayake <151937260+malithpromo@users.noreply.github.com>
2024-08-10 21:39:31 +0530

    login

--- passport.js
+++ passport.js
@@ -1,25 +1,117 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      // redirect URL
-      callbackURL: "http://localhost:4005/api/auth/google/callback",
-      scope: ["profile", "email"],
-    },
-    function (accessToken, refreshToken, profile, callback) {
-      callback(null, profile);
+// Assuming the Customer model is located in the following path
+const Customer = require("./src/models/customer"); // Update the path if necessary
+
+module.exports = function(passport) {
+  // Local Strategy
+  passport.use(
+    new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+      try {
+        const customer = await Customer.findOne({ email });
+        if (!customer) {
+          return done(null, false, { message: "Incorrect email." });
+        }
+        const isMatch = await bcrypt.compare(password, customer.password);
+        if (!isMatch) {
+          return done(null, false, { message: "Incorrect password." });
+        }
+        
+        // Generate JWT token
+        const token = jwt.sign({ sub: customer.id, email: customer.email }, process.env.JWT_SECRET, {
+          expiresIn: '1h'
+        });
+
+        
+        // Store token in session
+        return done(null, { customer, token });
+      } catch (err) {
+        return done(err);
+      }
+    })
+  );
+
+  // Google Strategy
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:4005/api/auth/google/callback",
+        scope: ["profile", "email"],
+      },
+      async (accessToken, refreshToken, profile, cb) => {
+        try {
+          let customer = await Customer.findOne({ googleId: profile.id });
+          if (!customer) {
+            customer = await Customer.create({
+              googleId: profile.id,
+              name: profile.displayName,
+              email: profile._json.email,
+              profilePhoto: profile.photos[0].value,
+            });
+          }
+          const token = jwt.sign({ sub: customer.id }, process.env.JWT_SECRET, {
+            expiresIn: '1h'
+          });
+          return cb(null, { customer, token });
+        } catch (err) {
+          console.error("Error during Google strategy authentication:", err);
+          return cb(err, null);
+        }
+      }
+    )
+  );
+
+  // JWT Strategy
+  // const cookieExtractor = (req) => {
+  //   let token = null;
+  //   if (req && req.cookies) {
+  //     token = req.cookies["access_token"];
+  //   }
+  //   return token;
+  // };
+
+  // passport.use(
+  //   new JwtStrategy(
+  //     {
+  //       jwtFromRequest: cookieExtractor,
+  //       secretOrKey: process.env.JWT_SECRET,
+  //     },
+  //     async (payload, done) => {
+  //       try {
+  //         const customer = await Customer.findById(payload.sub);
+  //         if (customer) {
+  //           return done(null, customer);
+  //         } else {
+  //           return done(null, false);
+  //         }
+  //       } catch (err) {
+  //         console.error("Error during JWT strategy authentication:", err);
+  //         return done(err, false);
+  //       }
+  //     }
+  //   )
+  // );
+
+  // Serialize user
+  passport.serializeUser((user, done) => {
+    done(null, user.customer.id);  // Assuming the user object has a customer field
+  });
+
+  // Deserialize user
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const customer = await Customer.findById(id);
+      done(null, customer);
+    } catch (err) {
+      done(err, null);
     }
-  )
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
+  });
+};
--- src/models/customer.js
+++ src/models/customer.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -19,10 +20,12 @@ const CustomerSchema = new Schema({
   gender: {
     type: String,
     required: false,
+    default: '',
   },
   address: {
     type: String,
     required: false,
+    default: '',
   },
   password: {
     type: String,
@@ -39,16 +42,47 @@ const CustomerSchema = new Schema({
   role: {
     type: String,
     default: "customer",
-  },    
+  },
   vehicle: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: "Vehicle",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
   googleId: {
     type: String,
+    unique: true,
+    sparse: true,
   },
 });
 
-const Customer = mongoose.model("Customer", CustomerSchema);
+// Pre-save hook to hash the password
+CustomerSchema.pre("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+    } catch (error) {
+      return next(error);
+    }
+  }
+  next();
+});
+
+// Method to compare passwords
+CustomerSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (error) {
+    throw new Error("Error comparing password: " + error.message);
+  }
+};
+
+// Check if the model already exists to prevent overwriting
+const Customer = mongoose.models.Customer || mongoose.model("Customer", CustomerSchema);
 
 module.exports = Customer;
